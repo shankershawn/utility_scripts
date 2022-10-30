355e9cde6a4d6b7fd42bf621631c0c17216a2b17
Shankarsan Ganai <shankershawn@gmail.com>
2022-10-30 16:03:03 +0530

    Added support to restrict email in case of no change in availability

--- availability.sh
+++ availability.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 get_availability_data () {
 	nanoseconds=$(date +%s%N | cut -b1-13)
+	isSendMail="N"
 	curl --location -o avail1.txt --request POST 'https://www.irctc.co.in/eticketing/protected/mapps1/avlFarenquiry/'$1'/'$2'/'$3'/'$4'/'$5'/'$6'/N' \
 	--header 'Accept:  application/json, text/plain, */*' \
 	--header 'bmirak:  webbm' \
@@ -16,8 +17,27 @@ get_availability_data () {
 	    "moreThanOneDay": true,
 	    "isLogedinReq": false
 	}'
-	cat avail1.txt | jq '{Timestamp: .timeStamp, TrainName: .trainName, TrainNo: .trainNo, From: .from, To: .to, Class: .enqClass, Quota: .quota}, {Status: .avlDayList[] | {DOJ: .availablityDate, Availability: .availablityStatus}}' | mail -s "Availability Status for $1 on $2 from $3 to $4 in class $5 in quota $6" $7
-	rm avail1.txt
+	if [ -f "avail_$1_$2_$3_$4_$5_$6.txt" ]
+	then
+		jq .avlDayList[] avail1.txt >> temp1.txt
+		jq .avlDayList[] avail_$1_$2_$3_$4_$5_$6.txt >> temp2.txt
+		diffVal=$(diff temp1.txt temp2.txt)
+		rm  temp1.txt temp2.txt
+		#diffVal="abc"
+		diffValLength=${#diffVal}
+		if [ $diffValLength -gt 0 ]
+		then
+			isSendMail="Y"
+		fi
+	else
+		isSendMail="Y"
+	fi
+
+	if [ "$isSendMail" = "Y" ]
+	then
+		cat avail1.txt | jq '{Timestamp: .timeStamp, TrainName: .trainName, TrainNo: .trainNo, From: .from, To: .to, Class: .enqClass, Quota: .quota}, {Status: .avlDayList[] | {DOJ: .availablityDate, Availability: .availablityStatus}}' | mail -s "Availability Status for $1 on $2 from $3 to $4 in class $5 in quota $6" $7
+	fi
+	mv avail1.txt avail_$1_$2_$3_$4_$5_$6.txt
 }
 
 while IFS= read -r line
