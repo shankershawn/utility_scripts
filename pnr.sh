001336a65aa7aacb1b698e754f750f58f05ce642
Shankarsan Ganai <shankershawn@gmail.com>
2025-08-18 23:38:34 +0530

    pointed pnr script to new api

--- pnr.sh
+++ pnr.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 for pnr in $(awk '{print $1}' pnrlist.txt)
 do
-	curl -s -o out1.txt https://www.confirmtkt.com/pnr-status/$pnr
-	trainNo=$(cat out1.txt | grep BookingStatusIndex | sed 's|data = ||' | sed 's|};|}|' | jq -r '.TrainNo')
+	curl 'https://cttrainsapi.confirmtkt.com/api/v2/ctpro/mweb/6156410112?querysource=ct-web&locale=en&getHighChanceText=true&livePnr=false'   -H 'Accept: */*'   -H 'Accept-Language: en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7'   -H $'ApiKey: ct-web\u00212$'   -H 'CT-Token;'   -H 'CT-Userkey;'   -H 'ClientId: ct-web'   -H 'Connection: keep-alive'   -H 'Content-Type: application/json'   -H 'DeviceId: a1b2355f-d43b-47f1-8b65-a95c09bc4887'   -H 'Origin: https://www.confirmtkt.com'   -H 'Referer: https://www.confirmtkt.com/'   -H 'Sec-Fetch-Dest: empty'   -H 'Sec-Fetch-Mode: cors'   -H 'Sec-Fetch-Site: same-site'   -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36'   -H 'sec-ch-ua: "Not)A;Brand";v="8", "Chromium";v="138", "Google Chrome";v="138"'   -H 'sec-ch-ua-mobile: ?0'   -H 'sec-ch-ua-platform: "macOS"'   --data-raw '{"proPlanName":"CP7","emailId":"","tempToken":""}' | jq -r '.data.pnrResponse' > out1.txt
+	trainNo=$(cat out1.txt | jq -r '.trainNo')
 	if [ ${#trainNo} -ne 0 ]
 	then
 		isSendMail="N"
 		emailReplyPrefix=""
-		trainName=$(cat out1.txt | grep BookingStatusIndex | sed 's|data = ||' | sed 's|};|}|' | jq -r '.TrainName')
-		doj=$(cat out1.txt | grep BookingStatusIndex | sed 's|data = ||' | sed 's|};|}|' | jq -r '.Doj')
-		boardingPoint=$(cat out1.txt | grep BookingStatusIndex | sed 's|data = ||' | sed 's|};|}|' | jq -r '.BoardingPoint')
-		reservationUpto=$(cat out1.txt | grep BookingStatusIndex | sed 's|data = ||' | sed 's|};|}|' | jq -r '.ReservationUpto')
-		class=$(cat out1.txt | grep BookingStatusIndex | sed 's|data = ||' | sed 's|};|}|' | jq -r '.Class')
-		cat out1.txt | grep BookingStatusIndex | sed 's|data = ||' | sed 's|};|}|' | jq -r '[.PassengerStatus[] | {Passenger: .Number, BookingStatus: .BookingStatus, CurrentStatus: .CurrentStatus, Prediction: .Prediction, ConfirmationStatus: .ConfirmTktStatus} | with_entries( select( .value != null ) )]' >> pnr_temp_$pnr.txt
+		trainName=$(cat out1.txt | jq '.trainName')
+		doj=$(cat out1.txt | jq '.doj')
+		boardingPoint=$(cat out1.txt | jq '.boardingPoint')
+		reservationUpto=$(cat out1.txt | jq '.reservationUpto')
+		class=$(cat out1.txt | jq '.class')
+		cat out1.txt | jq -r '[.passengerStatus[] | {Passenger: .number, BookingStatus: .bookingStatus, CurrentStatus: .currentStatus} | with_entries( select( .value != null ) )]' >> pnr_temp_$pnr.txt
 		if [ -f "pnr_data_$pnr.txt" ]
 		then
 			emailReplyPrefix="Re:"
@@ -28,7 +28,7 @@ do
 		mv pnr_temp_$pnr.txt pnr_data_$pnr.txt
 		if [ "$isSendMail" = "Y" ]
 		then
-			cat pnr_data_$pnr.txt | mail -s "$emailReplyPrefix PNR $pnr - $trainNo - $trainName - $doj - $boardingPoint to $reservationUpto - $class" shankershawn@gmail.com,nairitamganai@gmail.com,tkganaintpc@gmail.com
+			cat pnr_data_$pnr.txt | mail -s "$emailReplyPrefix PNR $pnr - $trainNo - $trainName - $doj - $boardingPoint to $reservationUpto - $class" shankarsan.ganai@icloud.com,nairitamganai@gmail.com,tkganaintpc@gmail.com
 			curl --location --request POST 'http://129.154.37.114:5001/v1/message/battery_level' \
 			--header 'Content-Type: application/json' \
 			--data-raw '{
